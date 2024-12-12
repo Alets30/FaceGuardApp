ef1775c856ce00559cadd87cd413b6631bd2880b
Alets30 <ettosn30@gmail.com>
2024-12-12 00:18:24 -0600

    Constantes de colores agregados

--- app/src/main/java/com/example/faceguardapp/Constantes.kt
+++ app/src/main/java/com/example/faceguardapp/Constantes.kt
@@ -2,4 +2,9 @@ package com.example.faceguardapp
 
 object Constantes {
     const val BASE_URL = "http://10.0.2.2:8000/"
+    const val PRIMARY_BLUE = 0xFF02416D
+    const val SECONDARY_BLUE = 0xFF0A5483
+    const val TERTIARY_BLUE = 0xFF066699
+    const val GREEN = 0xFFAEDD2B
+    const val WHITE = 0xFFF8F8EC
 }
